028f3de36da7d254d084a84157ac880e6271e44c
Albinson Arias Almonte <arias.almonte@gmail.com>
2020-08-01 20:42:49 -0400

    Crear clases y agregar reglas.txt

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print ("Juego de Domino")
+class Player:
+    
+    def __init__(self, id, fichas):
+        pass
+
+    def play():
+        pass
+
+    def win():
+        pass
+
+
+class Ficha():
+
+    def __init__(self, value1, value2, id):
+        self.id = id
+        self.value1 = value1
+        self.value2 = value2
+
+
+class Tablero():
+    arriba = []
+    abajo = []
