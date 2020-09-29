92334dd839c7db214c935d693b1fb767b3af8355
Albinson Arias <arias.almonte@gmail.com>
2020-09-29 17:33:43 -0400

    agregar clase Fichas

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ class Tablero:
     def verMesa (self):
         return self.tablero
 
-    def jugarFichaAlFinal(self, ficha):
+    def jugarFichaAlFinal (self, ficha):
         self.tablero.append(ficha)
 
     def jugarFichaAlInicio  (self, ficha):
         self.tablero.insert(0, ficha)
+
+class Fichas:
+
+    def __init__(self, valor1, valor2):
+        self.valor1, self.valor2 = valor1, valor2
+
+    def __repr__ (self):
+        return "[{},{}]".format(self.valor1,self.valor2)
