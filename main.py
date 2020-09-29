d77db5bd4e069e088a5633942cc3ebc8dde2198e
Albinson Arias <arias.almonte@gmail.com>
2020-09-29 18:20:15 -0400

    Agregar clase Jugador y metodos base

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 class Tablero:
 
-    def __init__(self):
+    def __init__ (self):
         self.tablero = []
 
-    
     def verMesa (self):
         return self.tablero
 
     def jugarFichaAlFinal (self, ficha):
         self.tablero.append(ficha)
 
-    def jugarFichaAlInicio  (self, ficha):
+    def jugarFichaAlInicio (self, ficha):
         self.tablero.insert(0, ficha)
+
 
 class Fichas:
 
-    def __init__(self, valor1, valor2):
+    def __init__ (self, valor1, valor2):
         self.valor1, self.valor2 = valor1, valor2
 
     def __repr__ (self):
@@ -23,3 +23,18 @@ class Fichas:
 
     def voltear (self):
         self.valor1, self.valor2 = self.valor2, self.valor1
+
+
+class Jugador:
+
+    def __init__ (self, nombre, fichas):
+        self.nombre, self.fichas = nombre, fichas
+
+    def __repr__ (self):
+        return self.nombre
+
+    def verMano (self):
+        return self.fichas
+
+    def jugarFicha (self, posicion):
+        return self.fichas.pop(posicion - 1)
