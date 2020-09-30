9278637e560110aebe7c2b565799d170c8a53127
Albinson Arias <arias.almonte@gmail.com>
2020-09-30 16:50:17 -0400

    Crear funciones para generar fichas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 class Tablero:
 
     def __init__ (self):
@@ -23,6 +25,26 @@ class Fichas:
 
     def voltear (self):
         self.valor1, self.valor2 = self.valor2, self.valor1
+
+    @classmethod
+    def from_list(cls, list):
+        return cls(list[0], list[1])
+
+    @staticmethod
+    def crearFichas():
+        lista = []
+        fichas = []
+        a = 7
+        b = 8
+        while (a > 0):
+            a -= 1
+            b -= 1
+            for y in range(b):
+                lista.append([a, y])
+        
+        for x in lista:
+            fichas.append(Fichas.from_list(x))
+        return fichas
 
 
 class Jugador:
